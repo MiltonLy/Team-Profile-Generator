d67edddf53bd379c5626332972b565a1a621063e
Milton Ly <ly.milton@gmail.com>
2023-01-26 22:58:54 -0800

    realized I didnt implement half my code, retyping index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const Manager = require('./roles/manager')
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-var employee = [];
+var employees = [];
 
-
+const managerCard = function(){
 inquirer
     .prompt([
         {
@@ -31,7 +31,18 @@ inquirer
         {
             message: 'What is your Email?',
             name: 'managerEmail',
-        },
+        }
+    ])
+    .then(managerData => {
+        const {managerName, managerJob, managerId, managerEmail, officeNumber} = managerData;
+        const manager = new Manager(managerName, managerJob, managerId, managerEmail, officeNumber);
+        employees.push(manager);
+    })
+}
+
+const engineerCard = function(){
+        inquirer
+        .prompt([
         {
             message: 'Who is your Engineer?',
             name: 'enginerName',
@@ -53,7 +64,18 @@ inquirer
         {
             message: ' What is your github username?',
             name: 'githubUrl'
-        },
+        }
+        ])
+        .then (engineerData => {
+            const {engineerName, engineerJob, engineerId, engineerEmail, githubUrl} = engineerData;
+            const engineer = new Engineer(engineerName, engineerJob, engineerId, engineerEmail, githubUrl);
+            employees.push(engineer)
+        })
+    }
+
+const internCard = function () {
+        inquirer
+        .prompt([
         {
             message: 'Who is this?',
             name: 'internName',
@@ -77,89 +99,9 @@ inquirer
             name: 'school'
         }
     ])
-    .then((response) => {
-        //all the data received written out
-        console.log (response); 
-        const responseStr = `
-        <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Team Portfolio</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
-    <header>
-        <h1>My Team</h1>
-    </header>
-<main>
-    <div class="flex-wrap">
-        <section>
-            <div class="container">
-                <div class="engineer">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="card-header bg-primary text-white">
-                            <h4 class="card-title">${data.engineerName}</h4>
-                            <h5 class="card-subtitle mb-2">${data.engineerJob}</h5>
-                            </div>
-                            <p></p>
-                            <h6 class="card-text">ID: ${data.engineerId}</h6>
-                            <h6 class="card-text">Email: <a href="mailto:${data.email}">${data.engineerEmail}</a></h6>
-                            <h6 class="card-text">GitHub: <a href="https://github.com/${data.github}"></a></h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <div class="container">
-                <div class="manager">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="card-header bg-primary text-white">
-                            <h4 class="card-title">${data.managerName}</h4>
-                            <h5 class="card-subtitle mb-2">${data.managerJob}</h5>
-                            </div>
-                            <p></p>
-                            <h6 class="card-text">ID: ${data.managerId}</h6>
-                            <h6 class="card-text">Email: <a href="mailto:${data.email}">${data.managerEmail}</a></h6>
-                            <h6 class="card-text">Office Number: ${data.officeNumber}</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section>
-            <div class="container">
-                <div class="intern">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="card-header bg-primary text-white">
-                            <h4 class="card-title">${data.internName}</h4>
-                            <h5 class="card-subtitle mb-2">${data.internJob}</h5>
-                            </div>
-                            <p></p>
-                            <h6 class="card-text">ID: ${data.internId}</h6>
-                            <h6 class="card-text">Email: <a href="mailto:${data.email}">${data.internEmail}</a></h6>
-                            <h6 class="card-text">School: ${data.school}</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-</div>
-</main> 
-</body>
-</html>`
-    fs.writeFile('template.html', responseStr, function(err) {
-        if (err) {
-            throw err;
-        }
+    .then(internData => {
+        const {internName, internJob, internId, internEmail, school} = internData
+        const intern = new Intern(internName, internJob, internId, internEmail, school);
+        employees.push(intern);
     })
-    });
+}
